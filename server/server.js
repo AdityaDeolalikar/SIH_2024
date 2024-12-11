68fc1865ce08b577a956cddcd53852c396a7397f
Atharva Deolalikar <atharvadeolalikar@gmail.com>
2024-12-11 23:34:39 +0530

    MOD | health check v2

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
